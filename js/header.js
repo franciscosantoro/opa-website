37d479a8477a60d3858f70dd2e7344d223ed4b8a
francisco <franciscosantoro@vera.com.uy>
2017-11-09 14:33:10 -0300

    se agrega el js para cambiar el color del header, todavía no funciona2

--- js/header.js
+++ js/header.js
@@ -1,7 +1,8 @@
-
+$(document).ready(function(){
+    
     setInterval(function() {
        $('.cabecera').animate( { backgroundColor: 'red' }, 300)
        .animate( { backgroundColor: 'green' }, 300); 
        }, 1000);
    
- 
+    )};
