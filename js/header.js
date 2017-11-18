0af21f95bfe3c1e2f92f25e60e654b4f563f553d
francisco <franciscosantoro@vera.com.uy>
2017-11-18 14:36:07 -0300

    script viejo borrado

--- js/header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    
-    setInterval(function() {
-       $('.cabecera').animate( { backgroundColor: 'red' }, 300)
-       .animate( { backgroundColor: 'green' }, 300); 
-       }, 1000);
-   
-    )};
