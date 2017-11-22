3ce56046d538f1d987646b9f00b461e7257633a6
francisco <franciscosantoro@vera.com.uy>
2017-11-22 20:28:19 -0300

    eliminado json-server de la carpeta, me da errores con git constantemente

--- js/login.js
+++ js/login.js
@@ -7,32 +7,38 @@ ourRequest.send();*/
 /* localStorage.setItem("username", nomregistro);
    localStorage.setItem("password", passregistro);*/
 
-function registro() {
-    var nomregistro = document.getElementById('nomregistro').value;
-    var passregistro = document.getElementById('passregistro').value;
-    alert ("dsuvysvidsvcdsivi!!!!!!1")
-    $.ajax({
-        type: 'POST',
-        url: 'http://localhost:3000/persona',
-        data: {
-            username: (nomregistro),
-            password: (passregistro)
-        },
-        success: function () {
-            document.getElementById("cerrate").click();
-        },
-        failure: function () {
-            alert("Error!");
-        }
-    });
-}
+   function registro() {
+    
+        var nomregistro = document.getElementById('nomregistro').value;
+    
+        var passregistro = document.getElementById('passregistro').value;
+
+        localStorage.setItem("username", nomregistro);
+
+        localStorage.setItem("password", passregistro);
+
+        $.ajax({
+    
+            type: 'POST',
+    
+            url: 'http://localhost:3000/usuario',
+    
+            data: { username: (nomregistro), password: (passregistro) },
+    
+            success: function () {document.getElementById("cerrate").click();},
+    
+            failure: function() {alert("Error!");}
+    
+        }); 
+    
+    }
 
 function login() {
     //cargo los datos del json en una variable
     var usuario = $.ajax({
         type: "GET",
         dataType : 'json',
-        url: 'http://localhost:3000/persona',
+        url: 'http://localhost:3000/usuario',
         success: function(){
             // Levanto los datos ingresados por el usuario
         var username = document.getElementById('username').value;
@@ -51,7 +57,7 @@ function login() {
     });
     
         
-
+}
 
 
 function mostrarNombre() {
