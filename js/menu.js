36047bee02e80a7f654d86ab4f6be6b3081fd006
blopasmart <31555762+blopasmart@users.noreply.github.com>
2017-11-29 22:03:44 -0300

    Jquery para stickmenu
    Jquery para menú pegajoso

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    var altura = $('.menu').offset().top;
+    
+    $(window).on('scroll', function(){
+
+        if ( $(window).scrollTop() > altura){
+            $('.menu').addClass('fixed');
+
+        } else {
+            $('.menu').removeClass('fixed');
+        }
+    });
+
+});
